22bed4b1ddaad4d241e4a718a73272d8b6983a09
Denis Maurin <denis.maurin@etu.utc.fr>
2022-06-07 14:42:22 +0200

    fix

--- pumpcontroller/PumpController.py
+++ pumpcontroller/PumpController.py
@@ -6,7 +6,7 @@ class PumpController:
         self.ser = serial.Serial("/dev/ttyUSB0", baudrate=9600)
 
     def command(self, tab):
-        self.ser.write(",".join([str(i) for i in tab]))
+        self.ser.write("-".join([str(i).rjust(2, "0") for i in tab]))
 
     def __del__(self):
         self.ser.close()
